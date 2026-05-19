491fcdf2e63bc8cfdac3dc561eb024eff3f0675e
Frida Padellaro <frida.padellaro@gmail.com>
2026-05-19 13:09:02 +0200

    Add use state for taskTitle

--- src/components/Board/Board.tsx
+++ src/components/Board/Board.tsx
@@ -5,6 +5,7 @@ import { useState } from "react";
 
 const Board = () => {
   const [showTaskForm, setShowTaskForm] = useState(false);
+  const [taskTitle, setTaskTitle] = useState(false);
 
   return (
     <main>
