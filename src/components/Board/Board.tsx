0bf15d8439a7604450a828d085ec995a6a93e517
MalinAlm <malin.therese.almqvist@gmail.com>
2026-05-19 13:02:14 +0200

    connect add-task-button  to create-task-form

--- src/components/Board/Board.tsx
+++ src/components/Board/Board.tsx
@@ -1,15 +1,25 @@
 import "./Board.css";
 import TaskCard from "../TaskCard/TaskCard";
 import CreateTaskForm from "../CreateTaskForm/CreateTaskForm";
+import { useState } from "react";
 
 const Board = () => {
+  const [showTaskForm, setShowTaskForm] = useState(false);
+
   return (
     <main>
       <h1>Test Board</h1>
       <div className="task-column-container">
         <div className="task-column todo-column">
           <h2>Todo</h2>
-          <button id="board-add-task-btn" className="board-add-task-btn">+ Add task</button>
+          <button
+            id="board-add-task-btn"
+            className="board-add-task-btn"
+            onClick={() => setShowTaskForm(true)}
+          >
+            + Add task
+          </button>
+          {showTaskForm && <CreateTaskForm />}
           <div>
             <ul>
               <TaskCard />
--- src/components/CreateTaskForm/CreateTaskForm.tsx
+++ src/components/CreateTaskForm/CreateTaskForm.tsx
@@ -1,14 +1,18 @@
 import "./CreateTaskForm.css";
 
 const CreateTaskForm = () => {
-    return (
-        <div className="create-task-form-container">
-            <div className="create-task-form">
-                <input id="create-task-form-input" className="create-task-form-input" type="text" />
-                <button id="create-task-form-btn">Create task</button>
-            </div>
-        </div>
-    );
+  return (
+    <div className="create-task-form-container">
+      <div className="create-task-form">
+        <input
+          id="create-task-form-input"
+          className="create-task-form-input"
+          type="text"
+        />
+        <button id="create-task-form-btn">Create task</button>
+      </div>
+    </div>
+  );
 };
 
 export default CreateTaskForm;
